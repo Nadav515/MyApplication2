6f961166f5e1194d722173f0c7ef30309d89131b
user <nadavsh515@gmail.com>
2024-03-29 13:05:29 +0300

    log in work

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,7 +4,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 // This is the main activity class for the application.
 // It extends AppCompatActivity and acts as the entry point for the app.
 // It sets up the user interface and handles user interactions
@@ -13,7 +19,17 @@ import androidx.appcompat.app.AppCompatActivity;
 // processing user input, and possibly integrating with DBHelper to save and retrieve data.
 public class MainActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
 
 
