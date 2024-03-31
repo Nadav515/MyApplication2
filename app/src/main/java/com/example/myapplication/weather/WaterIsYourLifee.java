a162ce4d0883bc87df77a91077e931e18418163a
user <nadavsh515@gmail.com>
2024-03-31 23:02:17 +0300

    finish matconet

---
+++ app/src/main/java/com/example/myapplication/weather/WaterIsYourLifee.java
@@ -0,0 +1,45 @@
+package com.example.myapplication.weather;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+import com.example.myapplication.SignUpActivity;
+
+public class WaterIsYourLifee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_water_is_your_lifee);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WaterIsYourLifee.this, SignUpActivity.class);
+                startActivities(new Intent[]{intent});
+                finish();
+            }
+
+            private void finish() {
+
+            }
+        }, 3000);
+    }
+}
